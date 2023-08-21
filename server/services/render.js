2e43815d811f40e6f4e9aa4bad37f5ac1f6e541f
mithrafaris <127948271+mithrafaris@users.noreply.github.com>
2023-08-21 16:05:32 +0530

    Project created

--- server/services/render.js
+++ server/services/render.js
@@ -1,24 +1,33 @@
-const axios=require('axios')//allow us to make request
-exports.userdetail=(req,res)=>{
-    //make a get request to/api/users
-    axios.get('http://localhost:3000/api/users')
-    .then(function(response){
-        console.log(response);
-        res.render('userdetails',{users:response.data})
-}).catch(err=>{
-    res.send(err)
-})
-}
+const axios = require("axios"); //allow us to make request
+exports.userdetail = (req, res) => {
+  //make a get request to/api/users
+  axios
+    .get("http://localhost:3000/api/users")
+    .then(function (response) {
+      res.render("userdetails", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
-exports.update=(req,res)=>{
-    res.render('update')
-}
-exports.admin=(req,res)=>{
-    res.render('admin')
-}
-exports.home=(req,res)=>{
-    res.render('index')
-}
-exports.registers=(req,res)=>{
-    res.render('signup')
-}
+exports.update = (req, res) => {
+  axios
+    .get("http://localhost:3000/api/users", { params: { id: req.query.id } })
+    .then(function (userdata) {
+      res.render("update", { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+exports.admin = (req, res) => {
+  res.render("admin");
+};
+exports.home = (req, res) => {
+  res.render("index");
+};
+exports.registers = (req, res) => {
+  res.render("signup");
+};
